df615ed13a6b02b72671fcff558defd9f4c578f6
Jacek Podkanski <ruby.object@googlemail.com>
2018-05-04 16:23:21 +0100

    main source file

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,21 @@
+  {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import qualified GI.Gtk as GI (init,
+                               main)
+import GI.Gtk (mainQuit,
+               onWidgetDestroy,
+               windowNew,
+               widgetShowAll)
+
+import GI.Gtk.Enums (WindowType(..))
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  _ <- GI.init Nothing
+
+  win <- windowNew WindowTypeToplevel
+  _ <- onWidgetDestroy win mainQuit
+  widgetShowAll win
+  GI.main
