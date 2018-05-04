99ae29cc0bfc3b784e3cf23dea2ff776214090df
Jacek Podkanski <ruby.object@googlemail.com>
2018-05-04 17:32:27 +0100

    problem with pulling cairo libraries

--- src/Main.hs
+++ src/Main.hs
@@ -4,18 +4,31 @@ module Main where
 
 import qualified GI.Gtk as GI (init,
                                main)
-import GI.Gtk (mainQuit,
-               onWidgetDestroy,
-               windowNew,
-               widgetShowAll)
+import GI.Gtk (
+  containerAdd
+  , drawingAreaNew
+  , onWidgetDestroy
+  , widgetShowAll
+  , windowNew
+  , mainQuit
+  )
+
+import GI.Cairo
+--import Graphics.Rendering.Cairo
 
 import GI.Gtk.Enums (WindowType(..))
+
+
 
 main :: IO ()
 main = do
   _ <- GI.init Nothing
 
   win <- windowNew WindowTypeToplevel
+
+  clock <- drawingAreaNew
+  containerAdd win clock
+
   _ <- onWidgetDestroy win mainQuit
   widgetShowAll win
   GI.main
