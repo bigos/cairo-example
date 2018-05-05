198c6fa0dfa5aa78cba0c0b9e5e8aafa32e92c0c
Jacek Podkanski <ruby.object@googlemail.com>
2018-05-05 12:59:46 +0100

    got my first cairo drawing

--- src/Main.hs
+++ src/Main.hs
@@ -2,33 +2,61 @@
 
 module Main where
 
-import qualified GI.Gtk as GI (init,
-                               main)
-import GI.Gtk (
-  containerAdd
-  , drawingAreaNew
-  , onWidgetDestroy
-  , widgetShowAll
-  , windowNew
-  , mainQuit
-  )
+import Control.Monad.Trans.Reader (runReaderT)
+import           Foreign.Ptr (castPtr)
+import qualified GI.Cairo as GICairo
+import qualified GI.GLib as GLib
+import qualified GI.Gtk as Gtk
+import qualified GI.Gdk as Gdk
 
-import GI.Cairo
---import Graphics.Rendering.Cairo
+import Graphics.Rendering.Cairo
+import Graphics.Rendering.Cairo.Internal (Render(runRender))
+import Graphics.Rendering.Cairo.Types (Cairo(Cairo))
 
 import GI.Gtk.Enums (WindowType(..))
 
+-- | This function bridges gi-cairo with the hand-written cairo
+-- package. It takes a `GI.Cairo.Context` (as it appears in gi-cairo),
+-- and a `Render` action (as in the cairo lib), and renders the
+-- `Render` action into the given context.
+renderWithContext :: GICairo.Context -> Render () -> IO ()
+renderWithContext ct r = Gdk.withManagedPtr ct $ \p ->
+  runReaderT (runRender r) (Cairo (castPtr p))
+
+updateCanvas :: Gtk.DrawingArea -> Render ()
+updateCanvas canvas = do
+  width'  <- fromIntegral <$> Gtk.widgetGetAllocatedWidth canvas
+  height' <- fromIntegral <$> Gtk.widgetGetAllocatedHeight canvas
+  let mwidth  = realToFrac width'
+      mheight = realToFrac height'
 
 
-main :: IO ()
+  setSourceRGB 0.6 0.9 0
+  setLineWidth 20
+  setLineCap LineCapRound
+  setLineJoin LineJoinRound
+
+  moveTo 30 30
+  lineTo (mwidth-30) (mheight-30)
+  lineTo (mwidth-30) 30
+  lineTo 30 (mheight-30)
+  stroke
+
 main = do
-  _ <- GI.init Nothing
+  _ <- Gtk.init  Nothing
 
-  win <- windowNew WindowTypeToplevel
+  win <- Gtk.windowNew WindowTypeToplevel
 
-  clock <- drawingAreaNew
-  containerAdd win clock
+  canvas <- Gtk.drawingAreaNew
 
-  _ <- onWidgetDestroy win mainQuit
-  widgetShowAll win
-  GI.main
+  _ <- Gtk.onWidgetDraw canvas $ \context ->
+    renderWithContext context (updateCanvas canvas) >> pure True
+
+
+  Gtk.containerAdd win canvas
+
+
+  _ <- Gtk.onWidgetDestroy win Gtk.mainQuit
+
+  Gtk.widgetShowAll win
+  Gtk.main
