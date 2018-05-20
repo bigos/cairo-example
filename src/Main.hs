83965f76417cd0e2270d80b7c44443bf2646078b
Jacek Podkanski <ruby.object@googlemail.com>
2018-05-20 20:57:16 +0100

    desugaring

--- src/Main.hs
+++ src/Main.hs
@@ -272,11 +272,15 @@ moveSnake2 model headingv =
 
 -- main ----------------------------------------
 
-timerFun g c = do
-  atomicModifyIORef' g $ \p -> do
-    (updateGlobalModel Tick p ,())
-  Gtk.widgetQueueDraw c
+timerFun g c = (atomicModifyIORef' g (\p -> (updateGlobalModel Tick p, ()))) >>
+  Gtk.widgetQueueDraw c >>
   return True
+
+-- timerFun g c = do
+--   atomicModifyIORef' g $ \p -> do
+--     (updateGlobalModel Tick p ,())
+--   Gtk.widgetQueueDraw c
+--   return True
 
 main :: IO ()
 main = do
